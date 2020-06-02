637d5d06b69793e600ec956bf159c6911973faf9
Cloud User <centos@ip-172-31-50-77.ec2.internal>
2020-06-02 16:31:46 +0000

    Solution to Ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name: Oskar Billington
+// Date: June 2nd 2020
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
@@ -20,11 +20,33 @@
 
 module counter(
     //Todo: add ports 
+	input clk,
+	input rst,
+	input enable,
+	input direction,
+	output [7:0] counter_out
 
     );
                     
     //Todo: add registers and wires, if needed
+	reg [7:0] counter_out;
 
     //Todo: add user logic
+
+//	*** Functionality NOT added: accounting for negative numbers and under-/overflow. 
+//	    Would be fairly easy with 2's complement and a flag respectively but not done here ***
+
+	always @ (posedge clk or posedge rst) begin
+		if (rst)
+			counter_out <= 8'b0;
+		else if (enable && direction)
+			counter_out <= counter_out + 1;
+		else if (enable)
+			counter_out <= counter_out - 1;
+		else
+			counter_out <= counter_out;
+	end	
+			
+
       
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name: Oskar Billington
+// Date: June 2nd 2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -14,15 +14,81 @@ module top_tb(
     );
     
 //Todo: Parameters
+parameter CLK_PERIOD = 10;
 
-//Todo: Regitsers and wires
+//Todo: Registers and wires
+reg clk;
+reg rst;
+reg enable;
+reg direction;
+wire [7:0] out;
+reg [7:0] solution;
+reg err;
 
 //Todo: Clock generation
+initial begin
+	clk = 1'b0;
+forever
+	#(CLK_PERIOD/2) clk = ~clk;
+end
 
-//Todo: User logic
-    
+//Todo: User logic      
+initial begin
+	err = 0;
+
+	// Test sequence:	
+	rst = 1; #1 // Reset to initialise, shouldn't have to be in sync with clk
+	rst = 0; #1
+	solution = 8'b0;
+	$display("Step1: Counter = %b",out);
+	if (out != solution) begin
+		err = 1;
+		$display("***Error in Step1***");
+	end
+
+	direction = 1;
+	enable = 1; 
+	#(5*CLK_PERIOD) // Count for 5 periods
+	enable = 0;
+	solution = 8'b00000101;
+	$display("Step2: Counter = %b",out);
+	if (out != solution) begin
+		err = 1;
+		$display("***Error in Step2***");
+	end
+
+	direction = 0;
+	enable = 1;
+	#(3*CLK_PERIOD) // Count down for 3 periods
+	enable = 0;
+	solution = 8'b00000010;
+	$display("Step3: Counter = %b",out);
+	if (out != solution) begin
+		err = 1;
+		$display("***Error in Step3***");
+	end
+
+	rst = 1; #1 // Reset
+	solution = 8'b0;
+	$display("Step4: Counter = %b",out);
+	if (out != solution) begin
+		err = 1;
+		$display("***Error in Step4***");
+	end
+
+
 //Todo: Finish test, check for success
+	if (err == 0) begin
+		$display("***TEST PASSED***");
+		$finish;
+	end	
+	else begin
+		$display("***TEST FAILED***");
+		$finish;
+	end	
+end
 
 //Todo: Instantiate counter module
+counter a0 [7:0] (.clk(clk), .rst(rst), .enable(enable), .direction(direction), .counter_out(out));
  
 endmodule 
