c2ea9feb57e93ab2fd7935caeac90a28bc97f92d
Cloud User <centos@ip-172-31-50-77.ec2.internal>
2020-06-04 15:50:37 +0000

    Solution in progress to Ex8

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Oskar Billington
+// Date: June 4th 2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,47 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+module AXI_timestable (
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input read, rst,
+	output wire [5:0] result
+	);
+
+		
+	// Intermediate wires for full 32-bit read-output
+	wire [31:0] s_axi_rdata;
+	assign result[5:0] = s_axi_rdata[5:0]; // Connect wires
+	always @ (posedge clk) begin
+		$display("Read data = %b",s_axi_rdata);
+	end
+	
+	// Instantiate BMEM with AXI4-lite interface
+	BMEM_AXI4 b0 [31:0] (
+  		.rsta_busy(),          // output wire rsta_busy
+  		.rstb_busy(),          // output wire rstb_busy
+  		.s_aclk(clk),                // input wire s_aclk
+  		.s_aresetn(!rst),          // input wire s_aresetn
+  		.s_axi_awaddr(32'b0),    // input wire [31 : 0] s_axi_awaddr
+  		.s_axi_awvalid(0),  // input wire s_axi_awvalid
+  		.s_axi_awready(),  // output wire s_axi_awready
+  		.s_axi_wdata(32'b0),      // input wire [31 : 0] s_axi_wdata
+  		.s_axi_wstrb(4'b0),      // input wire [3 : 0] s_axi_wstrb
+  		.s_axi_wvalid(0),    // input wire s_axi_wvalid
+  		.s_axi_wready(),    // output wire s_axi_wready
+  		.s_axi_bresp(),      // output wire [1 : 0] s_axi_bresp
+  		.s_axi_bvalid(),    // output wire s_axi_bvalid
+  		.s_axi_bready(1),    // input wire s_axi_bready
+  		.s_axi_araddr({26'b0, a, b}),    // input wire [31 : 0] s_axi_araddr
+  		.s_axi_arvalid(1),  // input wire s_axi_arvalid
+  		.s_axi_arready(),  // output wire s_axi_arready
+  		.s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata
+  		.s_axi_rresp(),      // output wire [1 : 0] s_axi_rresp
+ 		.s_axi_rvalid(),    // output wire s_axi_rvalid
+  		.s_axi_rready(read)    // input wire s_axi_rready
+		);
+
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,81 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Oskar Billington
+// Date: June 4th 2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+	parameter CLK_PERIOD = 10;
+
+	reg clk, read, rst, err;
+	reg [2:0] fac1;
+	reg [2:0] fac2;
+	wire [5:0] product;
+	reg [5:0] solution;
+
+	initial begin clk = 1'b0;
+		forever #(CLK_PERIOD/2) clk = ~clk;
+	end
+
+	// Instantiate timestable module
+	AXI_timestable p0 [5:0] (.clk(clk), .rst(rst), .read(read), .a(fac1), .b(fac2), .result(product));
+
+	// User logic
+	initial begin
+		rst = 0; 
+		err = 0;
+		read = 1;		
+
+		// Test sequence: 
+		fac1 = 1;
+		fac2 = 2;
+		#(5*CLK_PERIOD) // Latency of 2 ticks
+		solution = 6'b000010;
+		$display("Step1: Factor1 = %b, Factor2 = %b, Output = %b", fac1, fac2, product);
+		if (product !== solution) begin
+			err = 1;
+			$display("***Error in Step1***");
+		end		
+
+		fac1 = 7;
+		fac2 = 6;  
+		#(5*CLK_PERIOD)		
+		solution = fac1*fac2; // b101010
+		$display("Step2: Factor1 = %b, Factor2 = %b, Output = %b", fac1, fac2, product);
+		if (product !== solution) begin
+			err = 1;
+			$display("***Error in Step2***");
+		end	
+
+		fac1 = 3;
+		fac2 = 4;  
+		#(5*CLK_PERIOD)		
+		solution = fac1*fac2; // b001100
+		$display("Step3: Factor1 = %b, Factor2 = %b, Output = %b", fac1, fac2, product);
+		if (product !== solution) begin
+			err = 1;
+			$display("***Error in Step3***");
+		end		
+		
+
+		// Finish test, check for success:
+		if (err == 0) begin
+			$display("***TEST PASSED***");
+			$finish;
+		end
+		else begin
+			$display("***TEST FAILED***");
+			$finish;
+		end
+
+
+	end
+
+endmodule
